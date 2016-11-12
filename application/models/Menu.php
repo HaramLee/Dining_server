7ff2f21e8d6232c3a5f171b4d34c8e3ebfb6d306
Haram Lee <haram.lee@hotmail.ca>
2016-11-12 12:04:04 -0800

    - Added rules for form values to be used for validation for editing menu items, including images. - Added session removal in index method to clear menu item session, so you won't get stuck seeing one menu. - Added error message method to show the user that any values in the form wasn't passed as a message. - Added add method and button to add new menu items to the database. - Added delete method and button to delete a menu item from the database. - Added save method to save the either new/edit menu item into the database. - Implemented to upload image in edit method.

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -8,4 +8,34 @@ class Menu extends MY_Model
 		parent::__construct();
 	}
 
+
+	function rules() {
+		$config = [
+			['field'=>'id', 'label'=>'Menu code', 'rules'=> 'required|integer'],
+			['field'=>'name', 'label'=>'Item name', 'rules'=> 'required'],
+			['field'=>'description', 'label'=>'Item description', 'rules'=> 'required|max_length[256]'],
+			['field'=>'price', 'label'=>'Item price', 'rules'=> 'required|decimal'],
+			['field'=>'picture', 'label'=>'Item picture', 'rules'=> 'required'],
+			['field'=>'category', 'label'=>'Menu category', 'rules'=> 'required']
+		];
+		return $config;
+	}
+
+	function rule_picture() {
+	$config_picture = [
+        'upload_path' => './images', // relative to front controller
+		'allowed_types' => 'gif|jpg|jpeg|png',
+		'max_size' => 100, // 100KB should be enough for our graphical menu
+		'max_width' => 256,
+		'max_height' => 256, // actually, we want exactly 256x256
+		'min_width' => 256,
+		'min_height' => 256, // fixed it
+		'remove_spaces' => TRUE, // eliminate any spaces in the name
+		'overwrite' => TRUE, // overwrite existing image
+    ];
+
+	return $config_picture;
+	}
+
+
 }
--- application/views/mtce.php
+++ application/views/mtce.php
@@ -4,11 +4,15 @@
 		<th>Name</th>
 		<th>Description</th>
 	</tr>
+
 	{items}
-	<tr>
-		<td><a class="btn btn-default" role="button" href="/crud/edit/{id}">{id}</a></td>
+		<tr>
+		<td><a class='btn btn-default' role="button" href="/crud/edit/{id}">{id}</a></td>
 		<td>{name}</td>
 		<td>{description}</td>
-	</tr>
-	{/items}
+		<td>{picture}</td>  
+		</tr>
+  	{/items}
+  	
 </table>
+<a class="btn btn-default" role="button" href="/crud/add">Add a new menu item</a>
